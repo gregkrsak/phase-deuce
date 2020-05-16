04d0989f3a7215b65b61f9c231f18c5b252cf80a
Greg Krsak <greg.krsak@gmail.com>
2020-05-15 21:54:06 -0700

    Returned log level to "info"

--- phase-deuce.py
+++ phase-deuce.py
@@ -209,7 +209,7 @@ class Application(Controller):
         Performs tasks for the Application instance that should happen on startup.
         """
         # Initialize the internal logger (unrelated to writing to .CSV files)
-        self.log = Log(LOG_LEVEL_DEBUG) #FIXME - Return to LOG_LEVEL_INFO before commit to staging
+        self.log = Log(LOG_LEVEL_INFO)
 
         result = True
         try:
